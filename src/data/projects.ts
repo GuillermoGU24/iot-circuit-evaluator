8c181472cf6bd6c1ac9445b8f87b476fb65627c5
GuillermoGU24 <jguillermogu24@gmail.com>
2025-08-31 18:35:42 -0500

    cambio restric

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,8 +3,8 @@ export const projects = {
     name: "Hola Mundo",
     correctConnections: [
       { from: "D13", to: "RES1" },
-      { from: "RES1", to: "LED1_ANODO(+)" }, // Coincide con ledPins
-      { from: "LED1_CATODO(-)", to: "GND" }, // Coincide con ledPins
+      { from: "RES1", to: "LED1_ANODO(+)" },
+      { from: "LED1_CATODO(-)", to: "GND" },
     ],
     description:
       "Construir el circuito introductorio “Hola Mundo” en Arduino, conectando un diodo LED a la placa Arduino UNO mediante una resistencia de protección. El cátodo del LED debe ir a tierra (GND) y el ánodo, a través de la resistencia, al pin digital 13.",
